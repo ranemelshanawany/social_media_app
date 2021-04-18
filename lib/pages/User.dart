90513cdb35c6331c45cefe09bde29c75325cbd8f
MohamedKassem95 <80948494+MohamedKassem95@users.noreply.github.com>
2021-04-18 22:16:22 +0300

    User //to be used later

---
+++ lib/pages/User.dart
@@ -0,0 +1,19 @@
+//Preferably used when firebase is implemented
+
+
+class User {
+  final String username;
+  final String email;
+  final String photoUrl;
+  final String displayName;
+  final String bio;
+
+  User ({
+    this.username,
+    this.email,
+    this.photoUrl,
+    this.displayName,
+    this.bio,
+    }
+   );
+ }
